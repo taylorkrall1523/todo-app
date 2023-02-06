50c56adb2389f608498322f8261d9cfcd0b402a1
taylorkrall1523 <taylorkrall@knights.ucf.edu>
2023-02-06 00:09:46 -0500

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("InitialMenuItems");
+
+  const addToList = () => {
+    let tempArr = list;
+    tempArr.push(value);
+    setList(tempArr);
+    setValue("")};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h2>SideBar 1</h2>
+      <ul>{list.length > 0 && list.map((item) => <li>{item} </li>)}</ul>
+      <input
+type="text"
+value={value}
+onChange={(e) => setValue(e.target.value)}
+/>{" "} <button onClick={addToList}> Click to Add </button>
     </div>
   );
 }
+
+
+
 
 export default App;
